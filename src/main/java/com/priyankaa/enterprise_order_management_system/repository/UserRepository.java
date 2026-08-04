522f2d4885779484f0c4fb58b91331ba9a49360e
Priyankaa K <priyankaa2k2@gmail.com>
2026-08-04 23:59:38 +0530

    Add UserRepository using Spring Data JPA

---
+++ src/main/java/com/priyankaa/enterprise_order_management_system/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.priyankaa.enterprise_order_management_system.repository;
+
+import com.priyankaa.enterprise_order_management_system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
